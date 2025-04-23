c2bcbcc7a0cfacad2ca9e64187056ef18ded3245
ionescu-nicoleta-andra <ionescu.nicoleta.andra@gmail.com>
2025-04-23 08:50:01 +0300

    made the ending logger fix

--- app/Services/ImportService/Jobs/InsertProductJob.php
+++ app/Services/ImportService/Jobs/InsertProductJob.php
@@ -9,19 +9,23 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class InsertProductJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $productToImport;
+    protected $endOfImport;
 
     /**
      * Create a new job instance.
      */
-    public function __construct($productToImport)
+    public function __construct($productToImport, $endOfImport=null)
     {
         $this->productToImport = $productToImport;
+        $this->endOfImport = $endOfImport;
+
     }
 
     /**
@@ -29,11 +33,24 @@ class InsertProductJob implements ShouldQueue
      */
     public function handle(): void
     {
-        $inserter = new ProdusArrayInserter($this->productToImport);
-        $valid = $inserter->valid();
-        if ($valid) {
-            $inserter->insertInDb();
+        if ($this->endOfImport!==null) {
+
+        $newProducts = Produs::where('new', true)->count();
+        $stockProducts = Produs::where('stock_update', true)->count();
+        $priceProducts = Produs::where('price_update', true)->count();
+
+        Log::channel('import_feed')->info(" Imported ".$newProducts." new products ");
+        Log::channel('import_feed')->info(" Updated ".$stockProducts." stock products ");
+        Log::channel('import_feed')->info(" Updated ".$priceProducts." price products ");
+
+        } else {
+            $inserter = new ProdusArrayInserter($this->productToImport);
+            $valid = $inserter->valid();
+            if ($valid) {
+                $inserter->insertInDb();
+            }
         }
+
 
     }
 }
--- app/Services/ImportService/Parsers/ParserImportXLSX.php
+++ app/Services/ImportService/Parsers/ParserImportXLSX.php
@@ -15,12 +15,14 @@ class ParserImportXLSX
         foreach ($reader->getSheetIterator() as $sheet) {
             foreach ($sheet->getRowIterator() as $row) {
                 if ($counterRows != 0) {
-                    dispatch(new InsertProductJob($row));
+                    dispatch(new InsertProductJob($row, null));
                 }
                 $counterRows++;
             }
         }
         $reader->close();
+
+        dispatch(new InsertProductJob([], "end_of_import"));
 
         Log::channel('import_feed')->info(" Parsed ".$counterRows." lines.");
         Log::channel('import_feed')->info(" Parser ended  at : ". date("Y-m-d H:i:s"));
